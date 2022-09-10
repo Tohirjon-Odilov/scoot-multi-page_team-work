35f3799c1e0ddae8e7f4561659799081df72cea1
Tohirjon-Odilov <shahanshoh819@gamil.com>
2022-09-10 14:52:32 +0500

    rebase

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 let menu = document.querySelector("#header-mobile")
 let openMenu = document.querySelector(".open")
 let closeMenu = document.querySelector(".close")
-// let btnHeader = document.querySelector(".btn-header")
+let btnHeader = document.querySelector(".header__btn")
 
 openMenu.addEventListener("click", () => {
   menu.classList.toggle("header-mobile")
-  document.querySelector(".btn-header").style.display = "block"
+  openMenu.classList.add("none")
+  closeMenu.classList.add("block")
+  // openMenu.style.display = 'none';
+  // closeMenu.style.display = 'block';
+
+  document.querySelector('.header__btn').style.left = '0px'
 })
+
+closeMenu.addEventListener("click", () => {
+  menu.classList.toggle("header-mobile")
+  openMenu.classList.remove("none")
+  closeMenu.classList.remove("block")
+  // openMenu.style.display = 'block';
+  // closeMenu.style.display = 'none';
+  document.querySelector('.header__btn').style.left = '-320px'
+})
+
+
+// onclick = ""
+
