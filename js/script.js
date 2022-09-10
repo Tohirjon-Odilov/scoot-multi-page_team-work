fc4fc19d5c6ec15f73b085ece2478b9e5ae3188e
Tohirjon-Odilov <shahanshoh819@gamil.com>
2022-09-10 13:50:19 +0500

    rebase

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let menu = document.querySelector("#header-mobile")
 let openMenu = document.querySelector(".open")
 let closeMenu = document.querySelector(".close")
+// let btnHeader = document.querySelector(".btn-header")
 
 openMenu.addEventListener("click", () => {
   menu.classList.toggle("header-mobile")
+  document.querySelector(".btn-header").style.display = "block"
 })
